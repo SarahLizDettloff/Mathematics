d3063cd223336df6240b0c47561dc37cffa4bc76
Sarah Liz Dettloff <Sarah.Liz.Dettloff@gmail.com>
2018-02-18 23:06:29 -0500

    Create volume_of_a_frustum.py

---
+++ volume_of_a_frustum.py
@@ -0,0 +1,12 @@
+def get_volume(height, lower_r, upper_r):
+    volume = ((1/ float(3)) * height) * (float ((upper_r**2) + (upper_r*lower_r) + (lower_r**2)))
+    print ("The volume of the frustum is: " + str(area))
+
+def main():
+    height = input("What is the height of the frustum?:\n")
+    lower_r = input("What is the width of the top of the frustum?:\n")
+    upper_r = input("What is the length of the bottom of the frustum?:\n")
+    get_volume(height, lower_r, upper_r)
+
+if __name__ == "__main__":
+    main()
